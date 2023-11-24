3c232a79010f1596751e80a953d9d1936b111294
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-11-24 19:32:23 +0300

    task 102 zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,42 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+
+/**
+ * whileplus - a function that runs forever and returns nothing
+ * Return: 0 in the end
+ */
+int whileplus(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - the entry to a program that creates 5 zombie process
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int child = 0;
+	pid_t pid;
+
+	while (child < 5)
+	{
+		pid = fork();
+		if (!pid)
+		{
+			break;
+		}
+		printf("Zombie process created, PID: %i\n", (int)pid);
+		child++;
+	}
+	if (pid != 0)
+	{
+		whileplus();
+	}
+	return (0);
+}
