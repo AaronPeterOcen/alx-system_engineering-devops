0d4cb5b12d7d96b2afe441d89189b05f2604d493
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-19 20:35:47 +0300

    task 2 updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # killmenow process
 exec { 'killmenow':
-    command => 'pkill -f killmenow',
-    path => '/usr/bin:/usr/sbin:/bin'
+    command => 'pkill -o -f killmenow',
+    path => ['/usr/bin', '/usr/sbin', '/bin']
 }
