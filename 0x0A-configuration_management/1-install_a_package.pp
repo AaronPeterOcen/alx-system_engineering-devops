be64c5108530e9f546f39d78bbfbe9e78c5cf8b4
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-19 20:24:46 +0300

    task 1 create flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-/* installs flask onto the system*/
-package { 'python3-pip':
-  ensure => present,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  require     => Package['python3-pip'],
-  refreshonly => true,
+# installs flask onto the system
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3'
 }
