b7cefdd798e175a3feea723da8270973fe934804
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-20 14:11:01 +0300

    task 1 update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
 # Install flask
+package {
+	'python3-pip':
+	ensure => installed
+}
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+  ensure   => '2.1.1',
+require => Package['python3-pip'],
+  provider => 'pip',
+}
+
+package {
+'Werkzeug':
+ensure => '2.2.2',
+require =>Package['python3-pip'],
+provider => 'pip',
 }
