e19f5df9ea4c274291981d2e3dfe4a84cfcb4be2
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-21 20:01:19 +0300

    tassk 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,14 @@
 #configure ssh
-file_line{'Turn off passwd auth':
-	path => '/etc/ssh/ssh_config'
-	line => 'PasswordAuthentication no'
+# Client configuration file
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
 }
 
-file_line{'Declare identity file':
-	path => '/etc/ssh/ssh_config',
-	line => 'IdentityFile ~/.ssh/school'
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
