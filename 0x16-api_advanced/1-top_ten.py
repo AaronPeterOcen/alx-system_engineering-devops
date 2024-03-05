bb34db0573bd486d8ce5def7d5ced8466fff4b8f
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-05 09:23:28 +0300

    update on the files

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,2 +1,11 @@
  #!/usr/bin/python3
 
+"""
+function that queries the Reddit API,
+prints the titles of the first 10 hot posts listed 
+for a given subreddit.
+"""
+
+def top_ten(subreddit):
+	if !subreddit:
+		return None
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,2 +1,10 @@
  #!/usr/bin/python3
 
+"""
+function that queries the Reddit API,
+returns a list containing the titles of all hot articles
+for a given subreddit.
+"""
+
+def recurse(subreddit, hot_list=[]):
+	
