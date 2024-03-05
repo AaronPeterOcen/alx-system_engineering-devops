57287321b9f0371f70cbfae97c6ebe575c196b21
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-05 18:59:02 +0300

    update on task 0 displaying the number of subreddit subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 
 """
- a function that queries the Reddit API and returns the number of subscribers 
+ a function that returns the number of subscribers 
  (not active users, total subscribers) 
  for a given subreddit. 
  If an invalid subreddit is given, 
  the function should return 0.
 """
+import requests
+
+# from sys import argv
+
 
 def number_of_subscribers(subreddit):
-	if !subreddit:
-		return 0
+    """
+    returns the number of subscribers
+    for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 
 """
 function that queries the Reddit API,
